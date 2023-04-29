e5d3d5daa15bc1f00acee23da6de08d19ddbc8e5
Ashley Saleem-west <BooleanWhale@users.noreply.github.com>
2023-04-29 22:55:44 +0100

    Sets up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
+import Home from './pages/Home.js'
+import Blog from './pages/Blog.js'
 import './App.css';
 
 function App() {
+  const getClassName = ({ isActive}:{ isActive: boolean }):string => {
+    return isActive ? "active" : "";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <NavLink to="/" className={getClassName}>Home</NavLink>
+        <NavLink to="/blog" className={getClassName}>About</NavLink>
+      </nav>
+      
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/blog" element={<Blog />}></Route>
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
